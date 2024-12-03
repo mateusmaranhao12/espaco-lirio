f91f1c5a2de4d5995e8c3af8f48f21ce516b5f0c
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2024-12-03 09:35:18 -0300

    Implement Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,38 @@
 <template>
-    <h1>Navbar</h1>
-  </template>
-  
-  <script lang="ts">
-  import { Options, Vue } from 'vue-class-component'
-  
-  @Options({
-    components: {
-    },
-  })
-  export default class Navbar extends Vue {}
-  </script>
-  
-  <style lang="scss">
-  </style>
-  
+  <nav class="navbar-espaco-lirio">
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" href="#">A Casa</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Espaço</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Serviços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Preços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Parceiros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Área do Cliente</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {
+  },
+})
+export default class Navbar extends Vue { }
+</script>
+
+<style lang="scss">
+@import '../scss/styles.scss';
+</style>
